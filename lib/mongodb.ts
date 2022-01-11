4226b40930ed0d2030f76415770e1d72219841b0
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2022-01-11 19:06:40 +0100

    Add EAN search to show changes over time (and main document) for one product

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,3 +1,25 @@
 import { MongoClient } from 'mongodb'
 
-const client = new MongoClient(process.env.MONGODB_URI)
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI is not set')
+}
+
+if (process.env.NODE_ENV === "development") {
+    // If in develompent environment, add database connection to globals to be accessible after hot reloads
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise;
+} else {
+    client = new MongoClient(uri, options);
+    clientPromise = client.connect();
+}
+
+export default clientPromise;
--- pages/produkter/[id].tsx
+++ pages/produkter/[id].tsx
@@ -1,20 +1,79 @@
 import { useRouter } from 'next/router'
+import clientPromise from '../../lib/mongodb'
 
 
-export default function Produkt() {
+export default function Produkt({ produkt, changes }) {
     const router = useRouter();
     const { id } = router.query;
+    let change_items = [];
     
-    return <h1>{id}</h1>
+    if (changes) {
+        for (let [index, value] of changes.entries()) {
+            change_items.push(
+                <pre className="border-8 border-red-400" key={index}>
+                    <strong>{new Date(value.timestamp).toLocaleString("no-NB", { timeZone: "Europe/Oslo" })}</strong>
+                    <br/>
+                    {JSON.stringify(value, null, 2)}
+                </pre>
+            )
+        }
+    }
+
+    return (
+        <>
+            <h1 className="text-2xl">Søker på produkt med EAN: {id}</h1>
+            <h1 className="text-2xl">Produkt navn: {produkt ? produkt.title : "Ingen produkt funnet" }</h1>
+            <div>
+                <pre>{JSON.stringify(produkt, null, 2)}</pre>
+            </div>
+        { changes && (
+            <div>
+                <h1 className="text-2xl">{change_items.length} Totale Endringer (nyest til eldst):</h1>
+                {change_items}
+            </div>
+        )}  
+        </>
+    );
 }
 
 export async function getServerSideProps({ params }) {
-
-    const request = await fetch(`/api/produkt/${params.id}`);
-    const data = request.json();
-
-    return {
-        props: { produkt: data },
+    const client = await clientPromise;
+    // Get the first product with the provided EAN (this will be used to store all information except price in the future)
+    const produkt = await client.db("meny")
+                                .collection("products_diff")
+                                .findOne(
+                                    {"metadata.ean": params.id.toString()},
+                                    {"projection": {"_id": 0}}
+                                );
+    // If no product was found, return nothing
+    if (produkt === null) {
+        return {
+            props: {}
+        }
     }
-
+    // Get all the changes for the product
+    const changes_cursor = client.db("meny")
+                                .collection("products_diff")
+                                .find(
+                                    {
+                                        "metadata.ean": params.id.toString(),
+                                    },
+                                    {"projection": {"_id": 0}, "sort": {"timestamp": -1}}
+                                );
+    
+    // Go through the list of changes and change 
+    let changes_arr = [];
+    for await (const item of changes_cursor) {
+        item.timestamp = item.timestamp.getTime();
+        changes_arr.push(item);
+    }
+    // console.log(await changes_cursor.count());
+    // console.log(changes_arr.length);
+    produkt.timestamp = produkt.timestamp.getTime();
+    return {
+        props: { 
+            produkt: produkt,
+            changes: changes_arr
+        },
+    }
 }

b7007354f8f9755eec34fcf88287f4e8192b5185
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2022-02-01 13:20:55 +0100

    Work on stuff

--- components/products/ProductView.tsx
+++ components/products/ProductView.tsx
@@ -2,8 +2,13 @@ import Loader from "@components/Loader";
 import { Product } from "@lib/types/product";
 import Image from "next/image";
 
-export default function ProductView({ name, image, description, price }: { name: string, image: string, description: string, price: number }) {
+
+export default function ProductView({ product }: { product: any }) {
     // Get image, name, price, and description
+    const name = product.title
+    const image = "https://res.cloudinary.com/norgesgruppen/image/upload/f_auto,q_50,w_320,h_320,c_pad/" + product.imageName
+    const description = product.subtitle 
+    const price = product.pricePerUnit
     return (
         <div>
             <Image src={image} alt={name} height={320} width={320} />
--- pages/produkter/[id].tsx
+++ pages/produkter/[id].tsx
@@ -93,7 +93,7 @@ export default function Produkt({ product, priceChanges }: { product: any, price
             }
                 <h1 className="text-2xl">Søker på produkt med EAN: {id}</h1>
                 <h1 className="text-2xl">Produkt navn: {product.title}</h1>
-                <ProductView name={product.title} image={"https://res.cloudinary.com/norgesgruppen/image/upload/f_auto,q_50,w_320,h_320,c_pad/" + product.imageName} description={product.subtitle} price={priceChanges[0].pricePerUnit} />
+                <ProductView product={product} />
 
                 {/* <div>
                     <pre>

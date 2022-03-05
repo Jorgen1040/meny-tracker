64aa81a7b5588be1ab75f3ec56ba5f0010f1ef8a
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2022-03-06 00:55:24 +0100

    Add sales percentage indicator

--- components/products/ProductTile.tsx
+++ components/products/ProductTile.tsx
@@ -1,13 +1,21 @@
-import clientPromise from "@lib/mongodb";
 import Image from "next/image";
 import Link from "next/link";
+import SaleIcon from "../../public/sale_sticker.svg";
 
 export default function ProductTile({ product }: { product: any }) {
     return (
         <div className="rounded-lg shadow-xl hover:shadow-2xl border border-gray-400 border-opacity-0 hover:border-opacity-20 flex flex-col transition-all">
             <div className="p-4 flex justify-center">
-                <Link href={`${product.ean}`} prefetch={false}>
+                <Link href={`/produkter/${product.ean}`} prefetch={false}>
                     <a className="relative w-32 h-32">
+                        { product.isOffer &&
+                            <span className="fill-red-500 absolute right-0 z-10 flex rotate-12">
+                                <span className="absolute left-0 right-0 text-center top-1/4 text-white">
+                                    {(((product.pricePerUnit-product.pricePerUnitOriginal)/product.pricePerUnitOriginal)*100).toFixed(0)}%
+                                </span>
+                                <SaleIcon width={48} height={48} />
+                            </span>
+                        }
                         <Image 
                             src={"https://bilder.ngdata.no/" + product.ean + "/meny/medium.jpg"}
                             alt={product.title}
@@ -18,7 +26,7 @@ export default function ProductTile({ product }: { product: any }) {
                 </Link>
             </div>
             <div className="p-2 flex-grow w-full">
-                <Link href={`${product.ean}`}>
+                <Link href={`/produkter/${product.ean}`}>
                     <a>
                         <h1 className="text-xl hover:underline">{product.title}</h1>
                     </a>
@@ -26,7 +34,6 @@ export default function ProductTile({ product }: { product: any }) {
                 <p className="text-sm">{product.subtitle}</p> 
             </div>
             <div className="relative p-2">
-                {/* TODO: Add sale prices (and a little icon on the image to indicate sales) */}
                 { product.isOffer &&
                     <p className="absolute -top-2 text-gray-400"><s>kr {product.pricePerUnitOriginal.toFixed(2)}</s></p>
                 }

611d52555811d0722dea715c206bf993ec3dc8e1
Jorgen1040 <26395020+Jorgen1040@users.noreply.github.com>
2022-05-02 23:34:35 +0200

    Change indent from 4 to 2

--- components/products/ProductTile.tsx
+++ components/products/ProductTile.tsx
@@ -5,46 +5,63 @@ import SaleIcon from "../../public/sale_sticker.svg";
 export default function ProductTile({ product }: { product: any }) {
     // TODO: Add image blur
     return (
-        <div className="rounded-lg shadow-xl hover:shadow-2xl border border-gray-400 border-opacity-0 hover:border-opacity-20 flex flex-col transition-all">
-            <div className="p-4 flex justify-center">
-                <Link href={`/produkter/${product.ean}`} prefetch={false}>
-                    <a className="relative w-32 h-32">
-                        {/* Percentage sticker */}
-                        { product.isOffer &&
-                            <span className="fill-red-500 absolute right-0 z-10 flex rotate-12">
-                                <span className="absolute left-0 right-0 text-center top-1/4 text-white">
-                                    {(((product.pricePerUnit-product.pricePerUnitOriginal)/product.pricePerUnitOriginal)*100).toFixed(0)}%
-                                </span>
-                                <SaleIcon width={48} height={48} />
-                            </span>
-                        }
-                        <Image 
-                            src={"https://bilder.ngdata.no/" + product.ean + "/meny/medium.jpg"}
-                            alt={product.title}
-                            layout="fill"
-                            objectFit="contain"
-                        />
-                    </a>
-                </Link>
-            </div>
-            <div className="p-2 flex-grow w-full">
-                <Link href={`/produkter/${product.ean}`}>
-                    <a>
-                        <h1 className="text-xl hover:underline">{product.title}</h1>
-                    </a>
-                </Link>
-                <p className="text-sm">{product.subtitle}</p> 
-            </div>
-            <div className="relative p-2">
-                { product.isOffer &&
-                    <p className="absolute -top-2 text-gray-400"><s>kr {product.pricePerUnitOriginal.toFixed(2).replace(".", ",")}</s></p>
+      <div className="rounded-lg shadow-xl hover:shadow-2xl border border-gray-400 border-opacity-0 hover:border-opacity-20 flex flex-col transition-all">
+        <div className="p-4 flex justify-center">
+          <Link href={`/produkter/${product.ean}`} prefetch={false}>
+            <a className="relative w-32 h-32">
+              {/* Percentage sticker */}
+              {product.isOffer && (
+                <span className="fill-red-500 absolute right-0 z-10 flex rotate-12">
+                  <span className="absolute left-0 right-0 text-center top-1/4 text-white">
+                    {(
+                      ((product.pricePerUnit - product.pricePerUnitOriginal) /
+                        product.pricePerUnitOriginal) *
+                      100
+                    ).toFixed(0)}
+                    %
+                  </span>
+                  <SaleIcon width={48} height={48} />
+                </span>
+              )}
+              <Image
+                src={
+                  "https://bilder.ngdata.no/" + product.ean + "/meny/medium.jpg"
                 }
-                <p className="text-xl">kr {product.pricePerUnit.toFixed(2).replace(".", ",")}</p>
-                { product.comparePricePerUnit ?
-                    <p className="text-sm text-gray-500">kr {product.comparePricePerUnit.toFixed(2).replace(".", ",")}/{product.compareUnit}</p>
-                : <div className="h-5"></div>
-                }
-            </div>
+                alt={product.title}
+                layout="fill"
+                objectFit="contain"
+              />
+            </a>
+          </Link>
         </div>
-    )
+        <div className="p-2 flex-grow w-full">
+          <Link href={`/produkter/${product.ean}`}>
+            <a>
+              <h1 className="text-xl hover:underline">{product.title}</h1>
+            </a>
+          </Link>
+          <p className="text-sm">{product.subtitle}</p>
+        </div>
+        <div className="relative p-2">
+          {product.isOffer && (
+            <p className="absolute -top-2 text-gray-400">
+              <s>
+                kr {product.pricePerUnitOriginal.toFixed(2).replace(".", ",")}
+              </s>
+            </p>
+          )}
+          <p className="text-xl">
+            kr {product.pricePerUnit.toFixed(2).replace(".", ",")}
+          </p>
+          {product.comparePricePerUnit ? (
+            <p className="text-sm text-gray-500">
+              kr {product.comparePricePerUnit.toFixed(2).replace(".", ",")}/
+              {product.compareUnit}
+            </p>
+          ) : (
+            <div className="h-5"></div>
+          )}
+        </div>
+      </div>
+    );
 }
--- pages/produkter/[id].tsx
+++ pages/produkter/[id].tsx
@@ -14,218 +14,243 @@ import { getClientBuildManifest } from 'next/dist/client/route-loader';
 
 
 const logger = pino({
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            level: "debug",
-        }
-    }
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+      level: "debug",
+    },
+  },
 });
 
 interface Change {
-    timestamp: number;
-    pricePerUnit: number;
-    isOffer: boolean;
+  timestamp: number;
+  pricePerUnit: number;
+  isOffer: boolean;
 }
 
-export default function Produkt({ product, priceChanges, associated }: { product: any, priceChanges: Change[], associated: any[] }) {
-    const router = useRouter();
-    const { id } = router.query;
-    if (associated) console.log(associated.length);
-    
-    // Handle loading page
-    if (router.isFallback) {
-        return (
-            <div className="grid place-items-center mt-80">
-                <Loader show />
-            </div>
-        )
-    }
+export default function Produkt({
+  product,
+  priceChanges,
+  associated,
+}: {
+  product: any;
+  priceChanges: Change[];
+  associated: any[];
+}) {
+  const router = useRouter();
+  const { id } = router.query;
+  if (associated) console.log(associated.length);
 
-    if (!product) {
-        return (
-            <div className="grid">
-                <div>
-                    <h1 className="text-center text-5xl text-red-400 my-32">Produktet finnes ikke</h1>
-                    {/* <h1 className="text-center">Leter du etter disse?</h1> */}
-                    {/* TODO: Vis liste med produkter via Atlas Search med query */}
-                </div>
-            </div>
-        )
-    }
-
-    // console.log(product);
-    // console.log(priceChanges);
-    // console.log(associated);
- 
-    // Iterate over priceChanges and add missing values
-    // If there is a change loop over the entire array again
-    for (let i = 0; i < priceChanges.length; i++) {
-        const change = priceChanges[i];
-        const nextChange = priceChanges[i + 1];
-        if (nextChange && nextChange.timestamp - change.timestamp > 86400000) {
-            priceChanges.splice(i + 1, 0, {
-                timestamp: change.timestamp + 86400000,
-                pricePerUnit: change.pricePerUnit,
-                isOffer: change.isOffer,
-            });
-        }
-        // Fill in data up to current date
-        if (!nextChange && Date.now() - change.timestamp > 86400000) {
-            priceChanges.splice(i + 1, 0, {
-                timestamp: change.timestamp + 86400000,
-                pricePerUnit: change.pricePerUnit,
-                isOffer: change.isOffer,
-            });
-        }
-    }
-    // Dedupe
-    priceChanges.forEach((change, index) => {
-        const prevChange = priceChanges[index - 1];
-        const nextChange = priceChanges[index + 1];
-        if (prevChange && nextChange && moment(change.timestamp).format('DD.MM.YY') === moment(nextChange.timestamp).format('DD.MM.YY')) {
-            // Remove the price that didn't change
-            if (prevChange.pricePerUnit === change.pricePerUnit && nextChange.pricePerUnit !== change.pricePerUnit) {
-                priceChanges.splice(index, 1);
-            }
-        }
-    });
-    // TODO: Show important times in graph (highlight areas with sales)
+  // Handle loading page
+  if (router.isFallback) {
     return (
-        <>
-            <Head>
-                <title>Priser for {product.title} - Meny Tracker</title>
-            </Head>
-            <ProductView product={product} />
-            <div>
-                <h1 className="text-2xl my-3">Prishistorikk</h1>
-                <ResponsiveContainer width="100%" height={300} >
-                    <AreaChart
-                        data={priceChanges}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="timestamp" tickFormatter={timeStr => moment(timeStr).format('DD.MM.YY')} />
-                        <YAxis dataKey="pricePerUnit" />
-                        <Tooltip separator=': ' labelFormatter={timeStr => moment(timeStr).format('DD.MM.YY')} />
-                        {/* linear or monotone? */}
-                        <Area type="monotone" dataKey="pricePerUnit" stroke="#8884d8" name="Pris" unit=" kr" />
-                        {/* <Area type="monotone" dataKey="isOffer" stroke="#82ca9d" name="Tilbud?" /> */}
-                    </AreaChart>
-                </ResponsiveContainer>
-            </div>
-            <div className="mb-12">
-                <h1 className="text-2xl my-3">Lignende produkter</h1>
-                {/* TODO: Find our own associated products in case they don't exist in database */}
-                { associated.length > 0 ?
-                    <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
-                        {
-                            associated.map((product: any, index: number) => (
-                                <ProductTile key={index} product={product} />
-                            ))
-                            
-                        }
-                    </div>
-                    :
-                    <h1 className="text-xl text-red-400">Fant ingen lignende produkter</h1>
-                }
-            </div>
-        </>
+      <div className="grid place-items-center mt-80">
+        <Loader show />
+      </div>
     );
+  }
+
+  if (!product) {
+    return (
+      <div className="grid">
+        <div>
+          <h1 className="text-center text-5xl text-red-400 my-32">
+            Produktet finnes ikke
+          </h1>
+          {/* <h1 className="text-center">Leter du etter disse?</h1> */}
+          {/* TODO: Vis liste med produkter via Atlas Search med query */}
+        </div>
+      </div>
+    );
+  }
+
+  // console.log(product);
+  // console.log(priceChanges);
+  // console.log(associated);
+
+  // Iterate over priceChanges and add missing values
+  // If there is a change loop over the entire array again
+  for (let i = 0; i < priceChanges.length; i++) {
+    const change = priceChanges[i];
+    const nextChange = priceChanges[i + 1];
+    if (nextChange && nextChange.timestamp - change.timestamp > 86400000) {
+      priceChanges.splice(i + 1, 0, {
+        timestamp: change.timestamp + 86400000,
+        pricePerUnit: change.pricePerUnit,
+        isOffer: change.isOffer,
+      });
+    }
+    // Fill in data up to current date
+    if (!nextChange && Date.now() - change.timestamp > 86400000) {
+      priceChanges.splice(i + 1, 0, {
+        timestamp: change.timestamp + 86400000,
+        pricePerUnit: change.pricePerUnit,
+        isOffer: change.isOffer,
+      });
+    }
+  }
+  // Dedupe
+  priceChanges.forEach((change, index) => {
+    const prevChange = priceChanges[index - 1];
+    const nextChange = priceChanges[index + 1];
+    if (
+      prevChange &&
+      nextChange &&
+      moment(change.timestamp).format("DD.MM.YY") ===
+        moment(nextChange.timestamp).format("DD.MM.YY")
+    ) {
+      // Remove the price that didn't change
+      if (
+        prevChange.pricePerUnit === change.pricePerUnit &&
+        nextChange.pricePerUnit !== change.pricePerUnit
+      ) {
+        priceChanges.splice(index, 1);
+      }
+    }
+  });
+  // TODO: Show important times in graph (highlight areas with sales)
+  return (
+    <>
+      <Head>
+        <title>Priser for {product.title} - Meny Tracker</title>
+      </Head>
+      <ProductView product={product} />
+      <div>
+        <h1 className="text-2xl my-3">Prishistorikk</h1>
+        <ResponsiveContainer width="100%" height={300}>
+          <AreaChart
+            data={priceChanges}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="timestamp"
+              tickFormatter={(timeStr) => moment(timeStr).format("DD.MM.YY")}
+            />
+            <YAxis dataKey="pricePerUnit" />
+            <Tooltip
+              separator=": "
+              labelFormatter={(timeStr) => moment(timeStr).format("DD.MM.YY")}
+            />
+            {/* linear or monotone? */}
+            <Area
+              type="monotone"
+              dataKey="pricePerUnit"
+              stroke="#8884d8"
+              name="Pris"
+              unit=" kr"
+            />
+            {/* <Area type="monotone" dataKey="isOffer" stroke="#82ca9d" name="Tilbud?" /> */}
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="mb-12">
+        <h1 className="text-2xl my-3">Lignende produkter</h1>
+        {/* TODO: Find our own associated products in case they don't exist in database */}
+        {associated.length > 0 ? (
+          <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-5">
+            {associated.map((product: any, index: number) => (
+              <ProductTile key={index} product={product} />
+            ))}
+          </div>
+        ) : (
+          <h1 className="text-xl text-red-400">
+            Fant ingen lignende produkter
+          </h1>
+        )}
+      </div>
+    </>
+  );
 }
 
 interface Params {
-    params: {
-        id: string;
-    }
+  params: {
+    id: string;
+  };
 }
 
 export async function getStaticProps({ params }: Params) {
-    const { id } = params;
-    logger.info("Getting produkt with EAN: " + id);
-    const client = await clientPromise;
-    // Get the first product with the provided EAN (this will be used to store all information except price in the future)
-    const product = await client.db("meny")
-                                .collection("products")
-                                .findOne(
-                                    {"ean": id},
-                                    {"projection": {"_id": 0}}
-                                );
-    
-    // If no product was found, return nothing
-    if (!product) {
-        return {
-            props: {}
-        }
-    }
-    // Get all associated product documents
-    const associated: any[] = [];
-    // ? not all products have an associated list for some reason (newly added products?)
-    if(product.associated) {
-        const associated_ids = product.associated.products.slice(0, 10);
-        const associatedProducts = await client.db("meny")
-                                                .collection("products")
-                                                .find(
-                                                    {"ean": {$in: associated_ids}},
-                                                    {"projection": {"_id": 0}}
-                                                )
-                                                .toArray();
-        associated.push(...associatedProducts);
-    }
+  const { id } = params;
+  logger.info("Getting produkt with EAN: " + id);
+  const client = await clientPromise;
+  // Get the first product with the provided EAN (this will be used to store all information except price in the future)
+  const product = await client
+    .db("meny")
+    .collection("products")
+    .findOne({ ean: id }, { projection: { _id: 0 } });
 
-    // Get all the price changes for the product
-    const prices_cursor = client.db("meny")
-                                .collection("prices")
-                                .find(
-                                    {"metadata.ean": id},
-                                    {"projection": {"_id": 0}}
-                                )
-                                .sort({"timestamp": 1});
-
-    // Generate priceChanges array
-    // TODO: Optimize this
-    let priceChanges: Change[] = await prices_cursor.map((item) => {
-        return {
-            timestamp: item.timestamp.getTime(),
-            pricePerUnit: item.pricePerUnit.toFixed(2).replace(".", ","),
-            isOffer: item.isOffer?.toString() === "true"
-        };
-    }).toArray();
-
+  // If no product was found, return nothing
+  if (!product) {
     return {
-        props: { 
-            product,
-            priceChanges,
-            associated
-        },
-        // Revalidate after 10 minutes
-        revalidate: 600
-    }
+      props: {},
+    };
+  }
+  // Get all associated product documents
+  const associated: any[] = [];
+  // ? not all products have an associated list for some reason (newly added products?)
+  if (product.associated) {
+    const associated_ids = product.associated.products.slice(0, 10);
+    const associatedProducts = await client
+      .db("meny")
+      .collection("products")
+      .find({ ean: { $in: associated_ids } }, { projection: { _id: 0 } })
+      .toArray();
+    associated.push(...associatedProducts);
+  }
+
+  // Get all the price changes for the product
+  const prices_cursor = client
+    .db("meny")
+    .collection("prices")
+    .find({ "metadata.ean": id }, { projection: { _id: 0 } })
+    .sort({ timestamp: 1 });
+
+  // Generate priceChanges array
+  // TODO: Optimize this
+  let priceChanges: Change[] = await prices_cursor
+    .map((item) => {
+      return {
+        timestamp: item.timestamp.getTime(),
+        pricePerUnit: item.pricePerUnit.toFixed(2).replace(".", ","),
+        isOffer: item.isOffer?.toString() === "true",
+      };
+    })
+    .toArray();
+
+  return {
+    props: {
+      product,
+      priceChanges,
+      associated,
+    },
+    // Revalidate after 10 minutes
+    revalidate: 600,
+  };
 }
 
 export async function getStaticPaths() {
-    // const client = await clientPromise;
-    // const products = client.db("meny")
-    //                        .collection("products")
-    //                        .find({}, { "projection": { "_id": 0, "ean": 1 }});
+  // const client = await clientPromise;
+  // const products = client.db("meny")
+  //                        .collection("products")
+  //                        .find({}, { "projection": { "_id": 0, "ean": 1 }});
 
-    // logger.info("Got database response")
-    // let paths = [];
-    // for await (const item of products) {
-    //     paths.push({params: {id: item.ean}});
-    // }
-    // logger.info("finished getting paths")
-    const paths = [{ params: { id: "2000467100006" }}]
+  // logger.info("Got database response")
+  // let paths = [];
+  // for await (const item of products) {
+  //     paths.push({params: {id: item.ean}});
+  // }
+  // logger.info("finished getting paths")
+  const paths = [{ params: { id: "2000467100006" } }];
 
-    return {
-        paths,
-        // Fallback true allows for a loading page while page gets generated
-        fallback: true
-    }
+  return {
+    paths,
+    // Fallback true allows for a loading page while page gets generated
+    fallback: true,
+  };
 }
